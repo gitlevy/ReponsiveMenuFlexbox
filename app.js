d96a680c08644b45b2140a84b2fd798911a849c9
gitlevy <96433598+gitlevy@users.noreply.github.com>
2022-02-13 17:12:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ logoMenu.addEventListener('click', () => {
     if(contItems.className === 'cont_items') {
         contItems.className += ' responsive';
         logoMenu.style.top = '10px';
-        logoMenu.src = '/ressources/close.svg';
+        logoMenu.src = './ressources/close.svg';
     } else {
         contItems.className = 'cont_items';
         logoMenu.style.top = '15px';
-        logoMenu.src = '/ressources/menu.svg';
+        logoMenu.src = './ressources/menu.svg';
     }
 })
 
 arrItems.forEach(item => {
     item.addEventListener('click', () => {
         contItems.className = 'cont_items';
-        logoMenu.src = '/ressources/menu.svg';
+        logoMenu.src = './ressources/menu.svg';
     })
 })
